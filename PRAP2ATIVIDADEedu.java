7f1643551a1caf53aec8e37f1b2d42a7322532c7
ninjaskill22 <65202290+ninjaskill22@users.noreply.github.com>
2020-05-11 21:30:42 -0300

    Create PRAP2ATIVIDADEedu.java

---
+++ PRAP2ATIVIDADEedu.java
@@ -0,0 +1,80 @@
+package com.example.agendiario;
+
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "Stresses
+
+
+
+public class Stress {
+
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "StressesId")
+    private int mId;
+
+    @Nullable
+    @ColumnInfo(name = "StressesDescription")
+    private String mDescription;
+
+    private string mSelfControl;
+
+    public Stress(@Nullable String description, String selfControl, int level, String creationDate) {
+        mDescription = description;
+        mSelfControl = selfControl;
+        mLevel = level;
+        mCreationDate = creationDate;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDiscription(String description) {
+        mDescription = description;
+    }
+
+    public String getSelfControl() {
+        return mSelfControl;
+    }
+
+    public void setSelfControl(String SelfControl) {
+        mSelfControl = selfControl;
+    }
+
+    public int getLevel() {
+        return mLevel;
+    }
+
+    public void setLevel(int level) {
+        mLevel = level;
+    }
+
+    public String getCreationDate() {
+        return mCreationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        mCreationDate = creationDate;
+    }
+
+    private int mLevel;
+    private String mCreationDate;
+
+}
